8b39d2a244ab788086107064596e2dee65b84ef5
ilayazaria <ilay162534@gmail.com>
2024-04-11 21:03:35 +0300

    ended with borders finally

--- Frontend/app.py
+++ Frontend/app.py
@@ -1,9 +1,10 @@
 import pygame
 import math
-import time
 from utils import scale_image, blit_rotate_center
 
 PARKING_LOT = pygame.image.load("imgs/ParkingLot.jpg")
+PARKING_LOT_BORDERS = pygame.image.load("imgs/ParkingLotBorders.png")
+PARKING_LOT_BORDERS_MASK = pygame.mask.from_surface(PARKING_LOT_BORDERS)
 CAR = scale_image(pygame.image.load("imgs/Car.png"), 0.17)
 
 WIDTH, HEIGHT = PARKING_LOT.get_width(), PARKING_LOT.get_height()
@@ -26,15 +27,21 @@ class AbstractCar:
         self.x, self.y = self.START_POS
         self.acceleration = 0.05
         self.brake = 0.08
+        self.curr_car = self.img
+        self.curr_car_pos = (0, 0)
+        self.can_bounce = 0
 
     def rotate(self, left=False, right=False):
-        if right:
-            self.angle -= self.rotation_vel
-        elif left:
-            self.angle += self.rotation_vel
+        if self.vel != 0:
+            if right:
+                self.angle -= self.rotation_vel * self.vel
+            elif left:
+                self.angle += self.rotation_vel * self.vel
 
     def draw(self, win):
-        blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+        curr_car, new_rect_pos = blit_rotate_center(win, self.img, (self.x, self.y), self.angle)
+        self.curr_car = curr_car
+        self.curr_car_pos = new_rect_pos
 
     def move_forward(self):
         self.vel = min(self.vel + self.acceleration, self.max_vel)
@@ -60,6 +67,17 @@ class AbstractCar:
         self.y -= vertical
         self.x -= horizontal
 
+    def collide(self, mask, win, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.curr_car)
+        offset = (int(self.curr_car_pos[0] - x), int(self.curr_car_pos[1] - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
+
+
+class ParkingCar(AbstractCar):
+    IMG = CAR
+    START_POS = (5, 5)
+
     def reduce_speed(self):
         if self.vel > 0:
             self.vel = max(self.vel - self.acceleration / 2.0, 0)
@@ -68,10 +86,9 @@ class AbstractCar:
             self.vel = min(self.vel + self.acceleration / 2.0, 0)
             self.move()
 
-
-class ParkingCar(AbstractCar):
-    IMG = CAR
-    START_POS = (180, 200)
+    def bounce(self):
+        self.vel = -0.5 * self.vel
+        self.move()
 
 
 def draw(win, images, parking_car):
@@ -84,7 +101,7 @@ def draw(win, images, parking_car):
 run = True
 clock = pygame.time.Clock()
 images = [(PARKING_LOT, (0, 0))]
-parking_car = ParkingCar(4, -4, 4)
+parking_car = ParkingCar(2, -2, 1)
 
 while run:
     clock.tick(FPS)
@@ -106,13 +123,19 @@ while run:
     if keys[pygame.K_UP] and parking_car.vel >= 0:
         moved = True
         parking_car.move_forward()
-    elif keys[pygame.K_DOWN] and parking_car.vel <= 0:
+    if keys[pygame.K_DOWN] and parking_car.vel <= 0:
         moved = True
         parking_car.move_backwards()
     elif keys[pygame.K_SPACE]:
         moved = True
         parking_car.brake_car()
-
     if not moved:
         parking_car.reduce_speed()
+
+    if parking_car.can_bounce == 0:
+        if parking_car.collide(PARKING_LOT_BORDERS_MASK, WINDOW) is not None:
+            parking_car.bounce()
+            parking_car.can_bounce = 3
+    else:
+        parking_car.can_bounce -= 1
 pygame.quit()
--- Frontend/utils.py
+++ Frontend/utils.py
@@ -10,3 +10,5 @@ def blit_rotate_center(win, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
     win.blit(rotated_image, new_rect.topleft)
+    pygame.draw.rect(win, (255, 0, 0), new_rect, 2)
+    return rotated_image, new_rect.topleft
